69fcf5be7f176b19cb6981a50e70b6251e61655f
moviedatascience <moviedatascience@users.noreply.github.com>
2019-10-03 19:07:34 -0400

    added some proof of concept utility functions

---
+++ lambdata_moviedatascience/__init__.py
@@ -0,0 +1,8 @@
+""" A collection of useful data science helper functions """
+
+import pandas as pd
+import numpy as np
+
+#sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
---
+++ lambdata_moviedatascience/df_utils.py
@@ -0,0 +1,5 @@
+""" utility functions for working with dataframes"""
+
+import pandas
+
+TEST_DF = pandas.DataFrame([1, 2, 3])
